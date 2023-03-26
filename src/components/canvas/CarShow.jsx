a42c454cdf9b61964d80bd7ea9d144e7e4be0b33
Michael Bennett <mvbennett9@gmail.com>
2023-03-26 17:12:28 -0400

    created spotlights and background color

--- src/components/canvas/CarShow.jsx
+++ src/components/canvas/CarShow.jsx
@@ -7,10 +7,31 @@ export default function CarShow() {
 
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
-      <mesh>
+      <color args={[0, 0, 0]} attach={'background'} />
+
+      <spotLight
+        color={[1, .25, .7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadowBias={-0.0001}
+      />
+
+      <spotLight
+        color={[.14, .5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadowBias={-0.0001}
+      />
+      {/* <mesh>
         <boxGeometry args={[1, 1, 1]} />
         <meshBasicMaterial color={'red'} />
-      </mesh>
+      </mesh> */}
     </>
   )
 }
