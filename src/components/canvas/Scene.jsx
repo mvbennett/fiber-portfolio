cbabd6561fce26f9e2da89aeaaad170f46ee1cf9
Michael Bennett <mvbennett9@gmail.com>
2023-03-26 14:05:52 -0400

    removed boilerplate code and tested basic object rendering

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload } from '@react-three/drei'
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props}>
+    <Canvas shadows {...props}>
       <directionalLight intensity={0.75} />
       <ambientLight intensity={0.75} />
       {children}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,22 +5,18 @@ import Instructions from '@/components/dom/Instructions'
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
 // https://github.com/pmndrs/react-three-next/issues/49
-const Logo = dynamic(() => import('@/components/canvas/Logo'), { ssr: false })
+const CarShow = dynamic(() => import('@/components/canvas/CarShow'), { ssr: false })
 
 // Dom components go here
 export default function Page(props) {
   return (
-    <Instructions>
-      This is a minimal starter for Nextjs + React-three-fiber and Threejs. Click on the{' '}
-      <span className='text-cyan-200'>atoms nucleus</span> to navigate to the{' '}
-      <span className='text-green-200'>/blob</span> page. OrbitControls are enabled by default.
-    </Instructions>
+    <></>
   )
 }
 
 // Canvas components go here
 // It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => <Logo scale={0.5} route='/blob' position-y={-1} />
+Page.canvas = (props) => <CarShow scale={0.5} route='/blob' position-y={-1} />
 
 export async function getStaticProps() {
   return { props: { title: 'Index' } }
